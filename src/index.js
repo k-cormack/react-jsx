970abb4edcd476b1964844940bad65a951909147
Keith Cormack <kcormack@definitivelogic.com>
2019-02-20 16:29:27 -0700

    Labeling methods

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ import ReactDOM from 'react-dom';
 
 // Create React component
 const App = function () { //or use the ES2015 syntax of '() =>'
-  return <div>Hi there!!!</div>
+
+function getButtonText() {
+  return 'Click Here Now!!';
+}
+
+const buttonText = 'click me!';
+const labelText = { text: 'object prop'}
+
+  return (
+    <div>
+      <label className="label" htmlFor="name"> {/* use 'className' to avoid the confusion/collision with ES2015 use of 'class' in a declaration, and 'htmlFor' b/c of 'for' loops */}
+        Enter Name:
+        {labelText.text}
+      </label>
+      <input id="name" type="text"/>
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+      {buttonText}
+      {getButtonText()}
+
+      </button>
+    </div>
+  );
 };
 
 // Take the React component and show it on the screen
